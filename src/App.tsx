3853c611c3d84cf29deaaeedf41b1dfa2a107e44
Rocky.C <cloki0610@gmail.com>
2022-10-19 08:07:31 +0800

    Add lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,49 @@
+import React, { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import { Layout } from "./components/Layout/Layout";
 import { Home } from "./components/Layout/Home";
-import { Dash } from "./pages/Dash";
-import { Counter } from "./pages/Counter";
-import { Bmi } from "./pages/Bmi";
-import { Calculator } from "./pages/Calculator";
-import { Dashboard } from "./pages/Dashboard";
-import { Wordle } from "./pages/Wordle";
-import { NotFound } from "./pages/NotFound";
+
+const Dash = React.lazy(() =>
+  import("./pages/Dash").then(({ Dash }) => ({ default: Dash }))
+);
+const Counter = React.lazy(() =>
+  import("./pages/Counter").then(({ Counter }) => ({ default: Counter }))
+);
+const Bmi = React.lazy(() =>
+  import("./pages/Bmi").then(({ Bmi }) => ({ default: Bmi }))
+);
+const Calculator = React.lazy(() =>
+  import("./pages/Calculator").then(({ Calculator }) => ({
+    default: Calculator,
+  }))
+);
+const Dashboard = React.lazy(() =>
+  import("./pages/Dashboard").then(({ Dashboard }) => ({ default: Dashboard }))
+);
+const Wordle = React.lazy(() =>
+  import("./pages/Wordle").then(({ Wordle }) => ({ default: Wordle }))
+);
+const NotFound = React.lazy(() =>
+  import("./pages/NotFound").then(({ NotFound }) => ({ default: NotFound }))
+);
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/dice-game" element={<Dash />} />
-          <Route path="/date-counter" element={<Counter />} />
-          <Route path="/bmi-calc" element={<Bmi />} />
-          <Route path="/calculator" element={<Calculator />} />
-          <Route path="/weather-board" element={<Dashboard />} />
-          <Route path="/wordle" element={<Wordle />} />
-        </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/dice-game" element={<Dash />} />
+            <Route path="/date-counter" element={<Counter />} />
+            <Route path="/bmi-calc" element={<Bmi />} />
+            <Route path="/calculator" element={<Calculator />} />
+            <Route path="/weather-board" element={<Dashboard />} />
+            <Route path="/wordle" element={<Wordle />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </>
   );
 }
