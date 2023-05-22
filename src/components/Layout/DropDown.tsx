9e64ce97f5e1fcec5a24072a79c435261b823cb4
Rocky.C <cloki0610@gmail.com>
2023-05-23 07:04:01 +0800

    Update navbar animation

--- src/components/Layout/DropDown.tsx
+++ src/components/Layout/DropDown.tsx
@@ -3,41 +3,45 @@ import { NavLink } from "react-router-dom";
 
 import type { DropDownType } from "../../interfaces/HomepageType";
 import { projectList } from "../../constants/content";
+import { navbar } from "../../utils/motion";
+import { motion } from "framer-motion";
 
 const DropDown = ({ title, active, setActive }: DropDownType) => {
-  const [toggle, setToggle] = useState<boolean>(false);
-  const onToggle = () => {
-    setToggle((prevState) => !prevState);
-  };
-  return (
-    <li className="relative">
-      <div
-        className={`${
-          active === title ? "text-white" : "text-secondary"
-        } hover:text-white text-[18px] font-medium cursor-pointer`}
-        onClick={onToggle}
-      >
-        {title}
-      </div>
-      {toggle && (
-        <ul className="p-6 black-gradient absolute my-2 min-w-[120px] z-10 rounded-xl">
-          {projectList.map((project) => (
-            <li className="py-3 text-secondary hover:text-white">
-              <NavLink
-                to={project.url}
-                onClick={() => {
-                  setActive(title);
-                  setToggle(false);
-                }}
-              >
-                {project.name}
-              </NavLink>
-            </li>
-          ))}
-        </ul>
-      )}
-    </li>
-  );
+    const [toggle, setToggle] = useState<boolean>(false);
+    const onToggle = () => {
+        setToggle((prevState) => !prevState);
+    };
+    return (
+        <li className="relative">
+            <div
+                className={`${
+                    active === title ? "text-white" : "text-secondary"
+                } hover:text-white text-[18px] font-medium cursor-pointer`}
+                onClick={onToggle}
+            >
+                {title}
+            </div>
+            <motion.ul
+                variants={navbar}
+                animate={toggle ? "show" : "hidden"}
+                className="p-6 black-gradient absolute my-2 min-w-[120px] z-10 rounded-xl"
+            >
+                {projectList.map((project) => (
+                    <li className="py-3 text-secondary hover:text-white">
+                        <NavLink
+                            to={project.url}
+                            onClick={() => {
+                                setActive(title);
+                                setToggle(false);
+                            }}
+                        >
+                            {project.name}
+                        </NavLink>
+                    </li>
+                ))}
+            </motion.ul>
+        </li>
+    );
 };
 
 export default DropDown;
--- src/utils/motion.ts
+++ src/utils/motion.ts
@@ -1,111 +1,136 @@
 export const textVariant = (delay: number = 0.5) => {
-  return {
-    hidden: {
-      y: -50,
-      opacity: 0,
-    },
-    show: {
-      y: 0,
-      opacity: 1,
-      transition: {
-        type: "spring",
-        duration: 1.25,
-        delay: delay,
-      },
-    },
-  };
+    return {
+        hidden: {
+            y: -50,
+            opacity: 0,
+        },
+        show: {
+            y: 0,
+            opacity: 1,
+            transition: {
+                type: "spring",
+                duration: 1.25,
+                delay: delay,
+            },
+        },
+    };
 };
 
 export const fadeIn = (
-  direction: string,
-  type: string,
-  delay: number,
-  duration: number
+    direction: string,
+    type: string,
+    delay: number,
+    duration: number
 ) => {
-  return {
-    hidden: {
-      x: direction === "left" ? 100 : direction === "right" ? -100 : 0,
-      y: direction === "up" ? 100 : direction === "down" ? -100 : 0,
-      opacity: 0,
-    },
-    show: {
-      x: 0,
-      y: 0,
-      opacity: 1,
-      transition: {
-        type: type,
-        delay: delay,
-        duration: duration,
-        ease: "easeOut",
-      },
-    },
-  };
+    return {
+        hidden: {
+            x: direction === "left" ? 100 : direction === "right" ? -100 : 0,
+            y: direction === "up" ? 100 : direction === "down" ? -100 : 0,
+            opacity: 0,
+        },
+        show: {
+            x: 0,
+            y: 0,
+            opacity: 1,
+            transition: {
+                type: type,
+                delay: delay,
+                duration: duration,
+                ease: "easeOut",
+            },
+        },
+    };
 };
 
 export const zoomIn = (delay: number, duration: number) => {
-  return {
-    hidden: {
-      scale: 0,
-      opacity: 0,
-    },
-    show: {
-      scale: 1,
-      opacity: 1,
-      transition: {
-        type: "tween",
-        delay: delay,
-        duration: duration,
-        ease: "easeOut",
-      },
-    },
-  };
+    return {
+        hidden: {
+            scale: 0,
+            opacity: 0,
+        },
+        show: {
+            scale: 1,
+            opacity: 1,
+            transition: {
+                type: "tween",
+                delay: delay,
+                duration: duration,
+                ease: "easeOut",
+            },
+        },
+    };
 };
 
 export const slideIn = (
-  direction: string,
-  type: string,
-  delay: number,
-  duration: number
+    direction: string,
+    type: string,
+    delay: number,
+    duration: number
 ) => {
-  return {
-    hidden: {
-      x: direction === "left" ? "-100%" : direction === "right" ? "100%" : 0,
-      y: direction === "up" ? "100%" : direction === "down" ? "100%" : 0,
-    },
-    show: {
-      x: 0,
-      y: 0,
-      transition: {
-        type: type,
-        delay: delay,
-        duration: duration,
-        ease: "easeOut",
-      },
-    },
-  };
+    return {
+        hidden: {
+            x:
+                direction === "left"
+                    ? "-100%"
+                    : direction === "right"
+                    ? "100%"
+                    : 0,
+            y: direction === "up" ? "100%" : direction === "down" ? "100%" : 0,
+        },
+        show: {
+            x: 0,
+            y: 0,
+            transition: {
+                type: type,
+                delay: delay,
+                duration: duration,
+                ease: "easeOut",
+            },
+        },
+    };
 };
 
 export const staggerContainer = (
-  staggerChildren: number = 0.5,
-  delayChildren: number = 0.5
+    staggerChildren: number = 0.5,
+    delayChildren: number = 0.5
 ) => {
-  return {
-    hidden: {},
-    show: {
-      transition: {
-        staggerChildren: staggerChildren,
-        delayChildren: delayChildren || 0,
-      },
-    },
-  };
+    return {
+        hidden: {},
+        show: {
+            transition: {
+                staggerChildren: staggerChildren,
+                delayChildren: delayChildren || 0,
+            },
+        },
+    };
 };
 
 export const container = {
-  hidden: { opacity: 0 },
-  show: {
-    opacity: 1,
-    transition: {
-      delayChildren: 0.5,
+    hidden: { opacity: 0 },
+    show: {
+        opacity: 1,
+        transition: {
+            delayChildren: 0.5,
+        },
     },
-  },
+};
+
+export const navbar = {
+    show: {
+        display: "block",
+        opacity: 1,
+        transition: {
+            staggerChildren: 0.5,
+            delayChildren: 0.5,
+        },
+    },
+    hidden: {
+        display: "none",
+        opacity: 0,
+        transition: {
+            staggerChildren: 0.05,
+            staggerDirection: -1,
+            when: "afterChildren",
+        },
+    },
 };
