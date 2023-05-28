116324ad5230e9f5d08bf80074c35bfc0b611a4f
Rocky.C <cloki0610@gmail.com>
2023-05-28 18:45:03 +0800

    Remove unused CSS, adjust constant naming

--- src/components/WordleClone/KeyboardArea.tsx
+++ src/components/WordleClone/KeyboardArea.tsx
@@ -1,13 +1,17 @@
 import { KeyboardButton } from "./KeyboardButton";
-import { actions } from "../../utils/keyboard-actions";
+import { ACTIONS } from "../../utils/keyboard-actions";
 import type { KeyboardAreaProps } from "../../interfaces/WordleTypes";
 
 export const KeyboardArea = ({ onClick }: KeyboardAreaProps) => {
-  return (
-    <div className="flex flex-wrap w-[400px] gap-1 mt-4">
-      {actions.map((action, i) => (
-        <KeyboardButton key={`k-${i}`} action={action} onClick={onClick} />
-      ))}
-    </div>
-  );
+    return (
+        <div className="flex flex-wrap w-[400px] gap-1 mt-4">
+            {ACTIONS.map((action, i) => (
+                <KeyboardButton
+                    key={`k-${i}`}
+                    action={action}
+                    onClick={onClick}
+                />
+            ))}
+        </div>
+    );
 };
--- src/hooks/useWordle.ts
+++ src/hooks/useWordle.ts
@@ -1,95 +1,106 @@
 import React, { KeyboardEvent, useEffect, useState } from "react";
-import { actions } from "../utils/keyboard-actions";
+import { ACTIONS } from "../utils/keyboard-actions";
 import { historyType, status } from "../interfaces/WordleTypes";
 
 export const useWordle = () => {
-  const [solution, setSolution] = useState<string>("");
-  const [turn, setTurn] = useState<number>(0);
-  const [currentGuess, setGuess] = useState<string[]>([]);
-  const [guessHistory, setGuessHistory] = useState<historyType[][]>([]);
-  const [isCorrect, setIsCorrect] = useState<boolean>(false);
+    const [solution, setSolution] = useState<string>("");
+    const [turn, setTurn] = useState<number>(0);
+    const [currentGuess, setGuess] = useState<string[]>([]);
+    const [guessHistory, setGuessHistory] = useState<historyType[][]>([]);
+    const [isCorrect, setIsCorrect] = useState<boolean>(false);
 
-  const formatGuess = (currentGuess: string[]) => {
-    return currentGuess.join("").toLowerCase();
-  };
+    const formatGuess = (currentGuess: string[]) => {
+        return currentGuess.join("").toLowerCase();
+    };
 
-  const addNewGuess = () => {
-    if (currentGuess.length !== 5) return;
-    let newHistory: historyType[] = [];
-    // Compare result
-    if (formatGuess(currentGuess) === solution) {
-      // If correct set isCorrect to true
-      for (let i in currentGuess) {
-        newHistory.push({ char: currentGuess[i], status: status.TRUE });
-      }
-      setIsCorrect(true);
-    } else {
-      // If incorrect check each char
-      for (let i in currentGuess) {
-        if (currentGuess[i].toLowerCase() === solution[i]) {
-          newHistory.push({ char: currentGuess[i], status: status.TRUE });
-        } else if (solution.split("").includes(currentGuess[i].toLowerCase())) {
-          newHistory.push({ char: currentGuess[i], status: status.GOOD });
+    const addNewGuess = () => {
+        if (currentGuess.length !== 5) return;
+        let newHistory: historyType[] = [];
+        // Compare result
+        if (formatGuess(currentGuess) === solution) {
+            // If correct set isCorrect to true
+            for (let i in currentGuess) {
+                newHistory.push({ char: currentGuess[i], status: status.TRUE });
+            }
+            setIsCorrect(true);
         } else {
-          newHistory.push({ char: currentGuess[i], status: status.BAD });
+            // If incorrect check each char
+            for (let i in currentGuess) {
+                if (currentGuess[i].toLowerCase() === solution[i]) {
+                    newHistory.push({
+                        char: currentGuess[i],
+                        status: status.TRUE,
+                    });
+                } else if (
+                    solution.split("").includes(currentGuess[i].toLowerCase())
+                ) {
+                    newHistory.push({
+                        char: currentGuess[i],
+                        status: status.GOOD,
+                    });
+                } else {
+                    newHistory.push({
+                        char: currentGuess[i],
+                        status: status.BAD,
+                    });
+                }
+            }
         }
-      }
-    }
-    // Then add current guess to guess histor
-    setGuess([]);
-    setGuessHistory((prevHist) => [...prevHist, newHistory]);
-    setTurn((prevTurn) => prevTurn + 1);
-  };
+        // Then add current guess to guess histor
+        setGuess([]);
+        setGuessHistory((prevHist) => [...prevHist, newHistory]);
+        setTurn((prevTurn) => prevTurn + 1);
+    };
 
-  const handleKeyup = (e: KeyboardEvent) => {};
+    const handleKeyup = (e: KeyboardEvent) => {};
 
-  const handleClick = (
-    e: React.MouseEvent<HTMLButtonElement>,
-    action: string
-  ) => {
-    if (actions.includes(action)) {
-      if (action === "Enter") {
-        if (currentGuess.length === 5) {
-          addNewGuess();
+    const handleClick = (
+        e: React.MouseEvent<HTMLButtonElement>,
+        action: string
+    ) => {
+        if (ACTIONS.includes(action)) {
+            if (action === "Enter") {
+                if (currentGuess.length === 5) {
+                    addNewGuess();
+                }
+                return;
+            }
+            if (action === "Backspace") {
+                setGuess((prevGuess) => {
+                    let newGuess = [...prevGuess];
+                    newGuess.pop();
+                    return newGuess;
+                });
+                return;
+            }
+            if (currentGuess.length < 5) {
+                setGuess((prevGuess) => [...prevGuess, action.toUpperCase()]);
+            }
         }
-        return;
-      }
-      if (action === "Backspace") {
-        setGuess((prevGuess) => {
-          let newGuess = [...prevGuess];
-          newGuess.pop();
-          return newGuess;
-        });
-        return;
-      }
-      if (currentGuess.length < 5) {
-        setGuess((prevGuess) => [...prevGuess, action.toUpperCase()]);
-      }
-    }
-  };
+    };
 
-  const reset = () => {
-    setTurn(0);
-    setGuess([]);
-    setGuessHistory([]);
-    setIsCorrect(false);
-  };
+    const reset = () => {
+        setTurn(0);
+        setGuess([]);
+        setGuessHistory([]);
+        setIsCorrect(false);
+    };
 
-  useEffect(() => {
-    reset();
-  }, []);
+    useEffect(() => {
+        reset();
+    }, []);
 
-  return {
-    solution,
-    currentGuess,
-    guessHistory,
-    turn,
-    isCorrect,
-    setSolution,
-    handleKeyup,
-    handleClick,
-    reset,
-  };
+    return {
+        solution,
+        currentGuess,
+        guessHistory,
+        turn,
+        isCorrect,
+        setSolution,
+        handleKeyup,
+        handleClick,
+        reset,
+    };
 };
 
 export default useWordle;
